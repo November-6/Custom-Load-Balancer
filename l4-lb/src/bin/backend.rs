17188e2aae4367bc8d49b0027d276265af9f6559
November-6 <abhinavprajapati351@gmail.com>
2025-12-29 20:19:56 +0530

    done

--- l4-lb/src/bin/backend.rs
+++ l4-lb/src/bin/backend.rs
@@ -9,6 +9,12 @@ async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind(&addr).await?;
     println!("Backend listening on {}", addr);
 
+
+// I have a TCP listener that accepts connections and gives me a socket and peer.
+// When I accept a connection, I want to handle it without blocking the accept loop, so I spawn a new async task using tokio::spawn.
+// This task runs concurrently with other tasks, and Tokio schedules it on available worker threads.
+// The async move block transfers ownership of only the variables used inside the task (like socket) so the task can safely outlive the current scope without ownership or lifetime issues.
+
     loop {
         let (mut socket, peer) = listener.accept().await?;
         println!("Accepted connection from {}", peer);
@@ -38,7 +44,7 @@ async fn main() -> std::io::Result<()> {
 
                 if socket.write_all(&buf[..n]).await.is_err() {
                     return;
-                }
+                } 
             }
         });
     }
